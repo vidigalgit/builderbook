cab421dba16853264a2f638a916b551cc9461252
Christian Vidigal <vidigal@fastchannel.com>
2024-04-02 10:30:55 -0300

    setup para conexão com o mongo db

--- book/2-begin/server/server.js
+++ book/2-begin/server/server.js
@@ -2,13 +2,22 @@
 const express = require('express');
 const next = require ('next');
 
+const mongoose = require ('mongoose');
+
+require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+mongoose.connect(MONGO_URL);
+
 const port = process.env.PORT || 8000;
 const ROOT_URL=`http://localhost:${port}`;
 
-const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
+// Next.js server prepared
 app.prepare().then(() => {
     const server = express();
   
@@ -19,8 +28,9 @@ app.prepare().then(() => {
   
     server.get('*', (req, res) => handle(req, res));
   
+    // starting express server
     server.listen(port, (err) => {
       if (err) throw err;
-      console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
+      console.log(`> Ready on ${ROOT_URL}`); 
     });
   });
